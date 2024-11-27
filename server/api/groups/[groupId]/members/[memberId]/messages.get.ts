094a48b0c52766867c3d559c75a2ba8fa95d6120
Filippo Orru <filippoorru@hotmail.com>
2024-11-27 21:53:04 +0100

    Fix messages.get

--- server/api/groups/[groupId]/members/[memberId]/messages.get.ts
+++ server/api/groups/[groupId]/members/[memberId]/messages.get.ts
@@ -1,13 +1,16 @@
-import type { ChatMessage } from "~/utils/types";
 import { Types } from 'mongoose';
 import { toClientMessage, type DbChatMessage } from "~/server/models/messages.schema";
 
 export default defineEventHandler(async (event) => {
   const { group, member } = await getGroupData(event);
+  const targetMemberId = getRouterParams(event).memberId;
+  if (!group.members.some(m => m.id === targetMemberId)) {
+    throw new Error("Target member not found in group");
+  }
 
   const messages: (DbChatMessage & { _id: Types.ObjectId })[] = await MongoMessages.find({
     groupId: group.id,
-    memberId: member.id,
+    memberId: targetMemberId,
   }).exec();
 
   return messages.map(m => toClientMessage(m));
